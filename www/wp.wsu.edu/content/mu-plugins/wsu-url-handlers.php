2d06d9f1404ff5da941c6caa9a09fe59ef3f17c7
jeremyfelt <jeremy.felt@gmail.com>
2013-08-02 16:04:05 -0700

    Filter home_url to remove /wordpress/ from the URL path

--- www/wp.wsu.edu/content/mu-plugins/wsu-url-handlers.php
+++ www/wp.wsu.edu/content/mu-plugins/wsu-url-handlers.php
@@ -13,5 +13,18 @@ Author URI: http://wsu.edu/
 
 add_filter( 'network_site_url', 'wsu_modify_network_site_url', 10, 3 );
 function wsu_modify_network_site_url( $url ) {
-		return str_replace( '/wp-admin/network/', '/wordpress/wp-admin/network/', $url );
+	return str_replace( '/wp-admin/network/', '/wordpress/wp-admin/network/', $url );
+}
+
+add_filter( 'home_url', 'wsu_modify_home_url', 10, 4 );
+function wsu_modify_home_url( $url ) {
+	$allowed_schemes = array( 'http', 'https' );
+	$url_parts = parse_url( $url );
+
+	if ( isset( $url_parts['path'] ) && isset( $url_parts['scheme'] ) && in_array( $url_parts['scheme'], $allowed_schemes ) &&  '/wordpress' === substr( $url_parts['path'], 0, 10 ) ) {
+		$url_parts['path'] = substr( $url_parts['path'], 10 );
+		return $url_parts['scheme'] . '://' .  $url_parts['host'] . $url_parts['path'];
+	}
+
+	return $url;
 }
