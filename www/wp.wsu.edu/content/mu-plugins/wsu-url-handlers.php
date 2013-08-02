a8ba062fb0490c9487cee5008aaab0c51caa5604
jeremyfelt <jeremy.felt@gmail.com>
2013-08-02 16:04:53 -0700

    Add placeholders for additional URL filters that may be necessary as we build this out

--- www/wp.wsu.edu/content/mu-plugins/wsu-url-handlers.php
+++ www/wp.wsu.edu/content/mu-plugins/wsu-url-handlers.php
@@ -28,3 +28,53 @@ function wsu_modify_home_url( $url ) {
 
 	return $url;
 }
+
+add_filter( 'admin_url', 'wsu_modify_admin_url', 10, 3 );
+function wsu_modify_admin_url( $url, $a, $b ) {
+	return $url;
+}
+
+add_filter( 'site_url', 'wsu_modify_site_url', 10, 4 );
+function wsu_modify_site_url( $url, $a, $b ) {
+	return $url;
+}
+
+add_filter( 'includes_url', 'wsu_modify_includes_url', 10, 2 );
+function wsu_modify_includes_url( $url, $a ) {
+	return $url;
+}
+
+add_filter( 'content_url', 'wsu_modify_content_url', 10, 2 );
+function wsu_modify_content_url( $url, $a ) {
+	return $url;
+}
+
+add_filter( 'plugins_url', 'wsu_modify_plugins_url', 10, 3 );
+function wsu_modify_plugins_url( $url, $a, $b ) {
+	return $url;
+}
+
+add_filter( 'network_home_url', 'wsu_modify_network_home_url', 10, 3 );
+function wsu_modify_network_home_url( $url, $a, $b ) {
+	return $url;
+}
+
+add_filter( 'network_admin_url', 'wsu_modify_network_admin_url', 10, 2 );
+function wsu_modify_network_admin_url( $url, $a ) {
+	return $url;
+}
+
+add_filter( 'user_admin_url', 'wsu_modify_user_admin_url', 10, 2 );
+function wsu_modify_user_admin_url( $url, $a ) {
+	return $url;
+}
+
+add_filter( 'user_dashboard_url', 'wsu_modify_user_dashboard_url', 10, 4 );
+function wsu_modify_user_dashboard_url( $url, $a, $b, $c ) {
+	return $url;
+}
+
+add_filter( 'edit_profile_url', 'wsu_modify_edit_profile_url', 10, 3 );
+function wsu_modify_edit_profile_url( $url, $a, $b ) {
+	return $url;
+}
