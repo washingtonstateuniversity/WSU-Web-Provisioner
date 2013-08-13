94ded4f18288748a36f98c101afce1d5bdf54117
jeremyfelt <jeremy.felt@gmail.com>
2013-08-13 09:33:26 -0700

    Store emails sent through WordPress locally to a log file
    Rather than dealing with local email, we can inspect the log file in the content directory to see what emails are being generated by WSUWP

---
+++ www/wp.wsu.edu/content/mu-plugins/wsu-log-wp-mail.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Log wp_mail()
+Version: 0.1
+Plugin URI: http://wsu.edu
+Description: Log emails sent through WordPress to a text file.
+Author: jeremyfelt, wsu.edu
+*/
+
+add_filter( 'wp_mail', 'wsu_log_wp_mail', 1 );
+/**
+ * Stores emails sent through WordPress to a log file in the content directory.
+ *
+ * @param array $args arguments passed to wp_mail()
+ *
+ * @return array arguments, unmodified
+ */
+function wsu_log_wp_mail( $args ) {
+
+	// Only do this in a specific local environment.
+	if ( defined( 'WSU_LOCAL_CONFIG' ) && true === WSU_LOCAL_CONFIG ) {
+		$log_message = "---MESSAGE---\n" . var_export( array( date('r'), $args ), true );
+		$fp = fopen( WP_CONTENT_DIR . '/log/wp-mail.log', 'a+' );
+		fwrite( $fp, $log_message );
+		fclose( $fp );
+	}
+
+	return $args;
+}
