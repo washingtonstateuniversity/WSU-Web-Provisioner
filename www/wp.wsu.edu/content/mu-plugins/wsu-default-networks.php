2ce893834ae4e4e357c5ff9a99794aed133df508
jeremyfelt <jeremy.felt@gmail.com>
2013-08-13 16:33:43 -0700

    Additional populations of test networks and sites

--- www/wp.wsu.edu/content/mu-plugins/wsu-default-networks.php
+++ www/wp.wsu.edu/content/mu-plugins/wsu-default-networks.php
@@ -4,33 +4,54 @@ add_action( 'init', 'wsu_populate_network' );
 function wsu_populate_network() {
 
 	// Only do this in a specific local environment.
-	if ( ! defined( 'WSU_LOCAL_CONFIG' ) || true !== WSU_LOCAL_CONFIG )
+	if ( ! defined( 'WSU_LOCAL_CONFIG' ) || true !== WSU_LOCAL_CONFIG || ! is_multisite() )
 		return;
 
 	require_once( ABSPATH . 'wp-admin/includes/schema.php' );
-	populate_network( 2, 'network1.wp.wsu.edu', 'wsuwp-dev@wp.wsu.edu', 'Network 1', '/', true );
-	populate_network( 3, 'network2.wp.wsu.edu', 'wsuwp-dev@wp.wsu.edu', 'Network 2', '/', true );
-	populate_network( 4, 'network3.wp.wsu.edu', 'wsuwp-dev@wp.wsu.edu', 'Network 3', '/', true );
 
 	/**
-	 * Things to copy from current populate_network()
-	 * 
-	$current_site = new stdClass;
-	$current_site->domain = $domain;
-	$current_site->path = $path;
-	$current_site->site_name = ucfirst( $domain );
-	$wpdb->insert( $wpdb->blogs, array( 'site_id' => $network_id, 'domain' => $domain, 'path' => $path, 'registered' => current_time( 'mysql' ) ) );
-	$current_site->blog_id = $blog_id = $wpdb->insert_id;
-	update_user_meta( $site_user->ID, 'source_domain', $domain );
-	update_user_meta( $site_user->ID, 'primary_blog', $blog_id );
-
-	if ( $subdomain_install )
-		$wp_rewrite->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%/' );
-	else
-		$wp_rewrite->set_permalink_structure( '/blog/%year%/%monthnum%/%day%/%postname%/' );
-
-	flush_rewrite_rules();
+	 * The core site is setup already at wp.wsu.edu, there should be no other site
+	 * assigned to this network as it will be used for managing global options and
+	 * other networks.
 	 */
+
+	/**
+	 * Create network1.wp.wsu.edu and two additional sites as subdomains on that
+	 * network. This is an example of a standard network that extends the core
+	 * wp.wsu.edu domain via subdomains.
+	 */
+	populate_network( 2, 'network1.wp.wsu.edu', 'wsuwp-dev@wp.wsu.edu', 'Network 1', '/', true );
+	wpmu_create_blog( 'network1.wp.wsu.edu', '/', 'Network 1', 1, '', 2 );
+	wpmu_create_blog( 'site1.network1.wp.wsu.edu', '/', 'Network 1 Site 1', 1, '', 2 );
+	wpmu_create_blog( 'site2.network1.wp.wsu.edu', '/', 'Network 1 Site 2', 1, '', 2 );
+
+	/**
+	 * Create network2.wp.wsu.edu and two additional sites as subfolders rather than
+	 * sub domains.
+	 */
+	populate_network( 3, 'network2.wp.wsu.edu', 'wsuwp-dev@wp.wsu.edu', 'Network 2', '/', true );
+	wpmu_create_blog( 'network2.wp.wsu.edu', '/', 'Network 2', 1, '', 3 );
+	wpmu_create_blog( 'network2.wp.wsu.edu', '/site1/', 'Network 2 Site 1', 1, '', 3 );
+	wpmu_create_blog( 'network2.wp.wsu.edu', '/site2/', 'Network 2 Site 2', 1, '', 3 );
+
+	/**
+	 * Create wp-school1.wsu.edu and two additional sites as subdomains on that
+	 * network. This is an example of a subdomain setup that does not use the core
+	 * wp.wsu.edu domain.
+	 */
+	populate_network( 4, 'wp-school1.wsu.edu', 'wsuwp-dev@wp.wsu.edu', 'School 1', '/', true );
+	wpmu_create_blog( 'wp-school1.wsu.edu', '/', 'School 1', 1, '', 4 );
+	wpmu_create_blog( 'site1.wp-school1.wsu.edu', '/', 'School 1 Site 1', 1, '', 4 );
+	wpmu_create_blog( 'site2.wp-school1.wsu.edu', '/', 'School 1 Site 2', 1, '', 4 );
+
+	/**
+	 * Create wp-school2.wsu.edu and two additional sites using subfolders rather than
+	 * sub domains.
+	 */
+	populate_network( 5, 'wp-school2.wsu.edu', 'wsuwp-dev@wp.wsu.ed', 'School 2', '/', true );
+	wpmu_create_blog( 'wp-school2.wsu.edu', '/', 'School 2', 1, '', 5 );
+	wpmu_create_blog( 'wp-school2.wsu.edu', '/site1/', 'School 2 Site 1', 1, '', 5 );
+	wpmu_create_blog( 'wp-school2.wsu.edu', '/site2/', 'School 2 Site 2', 1, '', 5 );
 }
 
 // filter would be activated via #25020
