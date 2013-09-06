74c8545e2d5694d14cefaca5d1f61c923c03ae70
jeremyfelt <jeremy.felt@gmail.com>
2013-09-06 16:32:51 -0700

    Add documentation for our efforts in sunrise.php
    Explain a bunch of intent for the sake of being explicit.
    
    Also remove an unused constand DOMAIN_MAPPING

--- www/wp.wsu.edu/content/sunrise.php
+++ www/wp.wsu.edu/content/sunrise.php
@@ -1,6 +1,38 @@
 <?php
 /**
- * The sunrise file for WSUWP
+ * Sunrise file for WSUWP
+ *
+ * The primary function of this sunrise file is as a router for the sites being accessed
+ * on various networks within the WSUWP system. As each site is accessed, we do our best
+ * to cache that information for future requests, which limits the load on the database
+ * for simple site lookups.
+ *
+ * This file is loaded before many things in WordPress.
+ *
+ * Our current expectation is that all domains are stored in the wp_site and wp_blogs
+ * tables without leading or trailing slashes. e.g. wp.wsu.edu
+ *
+ * We also expect that all paths stored in the wp_site and wp_blogs tables do have
+ * leading and trailing slashes. e.g. /sub-site-path/
+ *
+ * Support is currently available for networks that are setup in subdomain mode and
+ * in subdirectory mode. Networks setup in subdirectory mode can only be one path deep.
+ *
+ *     These work:
+ *         - http://site1.network1.wp.wsu.edu/
+ *         - http://network2.wp.wsu.edu/site1/
+ *
+ *     This does not work:
+ *         - http://network2/wp.wsu.edu/site1/site2/
+ *
+ * When this file has done its intended job, several things will have been setup for use
+ * by the remaining parts of WordPress:
+ *
+ *     - $blog_id
+ *     - $site_id
+ *     - $current_blog
+ *     - $current_site
+ *     - COOKIE_DOMAIN
  *
  * @type WPDB $wpdb
  */
@@ -8,16 +40,15 @@
 if ( defined( 'COOKIE_DOMAIN' ) )
 	die( 'The constant "COOKIE_DOMAIN" is defined (probably in wp-config.php). Please remove or comment out that define() line.' );
 
-// capture the current domain request
+//Capture the domain and path from the current request
 $requested_domain    = $_SERVER['HTTP_HOST'];
 $requested_uri       = trim( $_SERVER['REQUEST_URI'], '/' );
 
-// If we only support one subfolder deep, then this will be it
-// otherwise, we may need to find a site option that matches the domain to indicate depth level
+// We currently support one subdirectory deep, and therefore only look at the first path level
 $requested_uri_parts = explode( '/', $requested_uri );
 $requested_path = $requested_uri_parts[0] . '/';
 
-// If we're dealing with a root domain, we want to leave it at /
+// If we're dealing with a root domain, we want to leave it at a path of '/'
 if ( '/' !== $requested_path )
 	$requested_path = '/' . $requested_path;
 
@@ -62,17 +93,18 @@ if ( ! $current_blog = wp_cache_get( 'wsuwp:site:' . $requested_domain . $reques
 	if( $found_site_id )
 		$current_blog = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE blog_id = %d LIMIT 1", $found_site_id ) );
 
+	// If a row was found, set it in cache for future lookups
 	if ( $current_blog )
 		wp_cache_set( 'wsuwp:site:' . $requested_domain . $requested_path, $current_blog, '', 60 * 60 * 12 );
 
 }
 
 if( $current_blog ) {
-
 	//set the blog_id and site_id globals that WordPress expects
 	$blog_id = $current_blog->blog_id;
 	$site_id = $current_blog->site_id;
 
+	// setup the current_site global that WordPress expects
 	if ( ! $current_site = wp_cache_get( 'wsuwp:network:' . $site_id ) ) {
 		$current_site = $wpdb->get_row( $wpdb->prepare( "SELECT * from $wpdb->site WHERE id = %d LIMIT 0,1", $site_id ) );
 
@@ -86,7 +118,6 @@ if( $current_blog ) {
 	}
 
 	define( 'COOKIE_DOMAIN', $requested_domain );
-	define( 'DOMAIN_MAPPING', 1 );
 } else {
 	/**
 	 * If we've made it here, the domain and path provided aren't doing us much good. At this
