3453feeb5c726dc966e0276dfe8f6fdc58a62f6f
jeremyfelt <jeremy.felt@gmail.com>
2013-09-06 16:02:30 -0700

    Implement object cache handling of site routing
    Take advantage of our object cache when querying for awareness of a specific site on the network. Stores and gets values for the site itself and for the network that the site belongs to. Theoretically the network will not need to be queried for often once it is in cache.
    
    We may need to consider a model where we store only the site ID for the domain/path combo as it's possible many key/value stores will be created for paths that do not actually have an individual site.

--- www/wp.wsu.edu/content/sunrise.php
+++ www/wp.wsu.edu/content/sunrise.php
@@ -21,59 +21,69 @@ $requested_path = $requested_uri_parts[0] . '/';
 if ( '/' !== $requested_path )
 	$requested_path = '/' . $requested_path;
 
-/** @todo implement an object cache to capture this */
+if ( ! $current_blog = wp_cache_get( 'wsuwp:site:' . $requested_domain . $requested_path ) ) {
+	// Treat www the same as the root URL
+	$alternate_domain = preg_replace( '|^www\.|', '', $requested_domain );
 
-// Treat www the same as the root URL
-$alternate_domain = preg_replace( '|^www\.|', '', $requested_domain );
+	//suppress errors and capture current suppression setting
+	$suppression = $wpdb->suppress_errors();
 
-//suppress errors and capture current suppression setting
-$suppression = $wpdb->suppress_errors();
+	if ( $requested_domain !== $alternate_domain )
+		$domain_where = $wpdb->prepare( "domain IN ( %s, %s )", $requested_domain, $alternate_domain );
+	else
+		$domain_where = $wpdb->prepare( "domain = %s", $requested_domain );
 
-if ( $requested_domain !== $alternate_domain )
-	$domain_where = $wpdb->prepare( "domain IN ( %s, %s )", $requested_domain, $alternate_domain );
-else
-	$domain_where = $wpdb->prepare( "domain = %s", $requested_domain );
-
-/**
- * The following query will find any one level deep subfolder sites on any page view, but
- * will only help us with subdomain networks if it is a root visit with an empty path. If
- * this returns null, we'll want to go to a backup.
- */
-$query = $wpdb->prepare( "SELECT blog_id FROM $wpdb->blogs WHERE $domain_where AND path = %s", $requested_path );
-$found_site_id = $wpdb->get_var( $query );
-
-/**
- * If the query for domain and path has failed, then we'll assume this is a site that has
- * no path assigned and search for that accordingly.
- */
-if ( ! $found_site_id ) {
-	$query = "SELECT blog_id FROM $wpdb->blogs WHERE $domain_where and path = '/' ";
+	/**
+	 * The following query will find any one level deep subfolder sites on any page view, but
+	 * will only help us with subdomain networks if it is a root visit with an empty path. If
+	 * this returns null, we'll want to go to a backup.
+	 */
+	$query = $wpdb->prepare( "SELECT blog_id FROM $wpdb->blogs WHERE $domain_where AND path = %s", $requested_path );
 	$found_site_id = $wpdb->get_var( $query );
+
+	/**
+	 * If the query for domain and path has failed, then we'll assume this is a site that has
+	 * no path assigned and search for that accordingly.
+	 */
+	if ( ! $found_site_id ) {
+		$query = "SELECT blog_id FROM $wpdb->blogs WHERE $domain_where and path = '/' ";
+		$found_site_id = $wpdb->get_var( $query );
+	}
+
+	//reset error suppression setting
+	$wpdb->suppress_errors( $suppression );
+
+	/**
+	 * If we found a blog_id to match the domain above, then we turn to WordPress to get the
+	 * remaining bits of info from the standard wp_blogs and wp_site tables. Then we squash
+	 * it all together in the $current_site, $current_blog, $site_id, and $blog_id globals so
+	 * that it is available for the remaining operations on this page request.
+	 */
+	if( $found_site_id )
+		$current_blog = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE blog_id = %d LIMIT 1", $found_site_id ) );
+
+	if ( $current_blog )
+		wp_cache_set( 'wsuwp:site:' . $requested_domain . $requested_path, $current_blog, '', 60 * 60 * 12 );
+
 }
 
-//reset error suppression setting
-$wpdb->suppress_errors( $suppression );
-
-/**
- * If we found a blog_id to match the domain above, then we turn to WordPress to get the
- * remaining bits of info from the standard wp_blogs and wp_site tables. Then we squash
- * it all together in the $current_site, $current_blog, $site_id, and $blog_id globals so
- * that it is available for the remaining operations on this page request.
- */
-if( $found_site_id ) {
-	$current_blog = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE blog_id = %d LIMIT 1", $found_site_id ) );
+if( $current_blog ) {
 
 	//set the blog_id and site_id globals that WordPress expects
-	$blog_id = $found_site_id;
+	$blog_id = $current_blog->blog_id;
 	$site_id = $current_blog->site_id;
 
-	$current_site = $wpdb->get_row( $wpdb->prepare( "SELECT * from $wpdb->site WHERE id = %d LIMIT 0,1", $site_id ) );
+	if ( ! $current_site = wp_cache_get( 'wsuwp:network:' . $site_id ) ) {
+		$current_site = $wpdb->get_row( $wpdb->prepare( "SELECT * from $wpdb->site WHERE id = %d LIMIT 0,1", $site_id ) );
 
-	// Add blog ID after the fact because it is required by both scenarios
-	$current_site->blog_id = $blog_id;
+		// Add blog ID after the fact because it is required by both scenarios
+		$current_site->blog_id = $blog_id;
 
-	// Attach the site name to our current_site object. This uses cache already.
-	$current_site = get_current_site_name( $current_site );
+		// Attach the site name to our current_site object. This uses cache already.
+		$current_site = get_current_site_name( $current_site );
+
+		wp_cache_set( 'wsuwp:network:' . $site_id, $current_site, '', 60 * 60 * 12 );
+	}
 
 	define( 'COOKIE_DOMAIN', $requested_domain );
 	define( 'DOMAIN_MAPPING', 1 );
