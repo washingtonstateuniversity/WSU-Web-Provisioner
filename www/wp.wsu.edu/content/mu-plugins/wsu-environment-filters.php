42676e74f971c75deb6f2af3e593df32e09de1fa
jeremyfelt <jeremy.felt@gmail.com>
2013-09-10 16:00:43 -0700

    Move got_rewrite filter to new environment filters file

--- www/wp.wsu.edu/content/mu-plugins/wsu-environment-filters.php
+++ www/wp.wsu.edu/content/mu-plugins/wsu-environment-filters.php
@@ -3,5 +3,15 @@
  * Filters specific to the environment provided by WSUWP
  */
 
+/**
+ * Remove `index.php` from permalinks when using Nginx
+ *
+ * If `index.php` is showing up as a level within your permalinks when running
+ * WordPress on Nginx, it can be removed by hooking into 'got_rewrite' and
+ * explicitly forcing WordPress to believe that rewrite is available.
+ *
+ */
+add_filter( 'got_rewrite', '__return_true', 999 );
+
 // Avoid generating .htaccess files whenever rewrite rules are flushed
 add_filter( 'flush_rewrite_rules_hard', '__return_false');
