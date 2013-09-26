0c7ad65bc309cbdce3798280fbf34f4482a02669
jeremyfelt <jeremy.felt@gmail.com>
2013-09-26 12:48:15 -0700

    Introduce WSU child themes for TwentyTwelve and TwentyThirteen

---
+++ www/wp.wsu.edu/content/themes/wsu-twentytwelve/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+</div><!-- #main .wrapper -->
+<footer id="colophon" role="contentinfo">
+	<div class="site-info">
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
