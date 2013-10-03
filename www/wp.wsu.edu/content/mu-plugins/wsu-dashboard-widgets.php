98acfa540a7a027bdf459f0c4b64750c9aefb7aa
jeremyfelt <jeremy.felt@gmail.com>
2013-10-03 13:20:47 +0200

    Move dev dashboard over to standard dashbaoard.

--- www/wp.wsu.edu/content/mu-plugins/wsu-dashboard-widgets.php
+++ www/wp.wsu.edu/content/mu-plugins/wsu-dashboard-widgets.php
@@ -24,3 +24,11 @@ function wsu_remove_dashboard_widgets() {
 
 	remove_action( 'welcome_panel', 'wp_welcome_panel' );
 }
+
+add_action( 'wp_network_dashboard_setup', 'wsu_remove_network_dashboard_widgets' );
+function wsu_remove_network_dashboard_widgets() {
+	remove_meta_box( 'network_dashboard_right_now', 'dashboard-network', 'normal' );
+	remove_meta_box( 'dashboard_plugins'          , 'dashboard-network', 'normal' );
+	remove_meta_box( 'dashboard_primary'          , 'dashboard-network', 'side'   );
+	remove_meta_box( 'dashboard_secondary'        , 'dashboard-network', 'side'   );
+}
