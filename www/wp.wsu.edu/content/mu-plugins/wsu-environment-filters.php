e0aa1fb92fe563a0ae4f0eeb8ab144f2f5d7733f
jeremyfelt <jeremy.felt@gmail.com>
2013-10-09 16:09:39 +0200

    Remove nginx related filters fixed in 3.7
    Before 3.7, WordPress was unaware that Nginx supported rewrites. Because of this, we would get some extra stuff whenever the permalinks were flushed and a confusing index.php popped up in the permalinks settings.
    
    Now that these issues have been resolved, we can remove these filters.

--- www/wp.wsu.edu/content/mu-plugins/wsu-environment-filters.php
+++ www/wp.wsu.edu/content/mu-plugins/wsu-environment-filters.php
@@ -8,19 +8,6 @@ Version: 0.1
 */
 
 /**
- * Remove `index.php` from permalinks when using Nginx
- *
- * If `index.php` is showing up as a level within your permalinks when running
- * WordPress on Nginx, it can be removed by hooking into 'got_rewrite' and
- * explicitly forcing WordPress to believe that rewrite is available.
- *
- */
-add_filter( 'got_rewrite', '__return_true', 999 );
-
-// Avoid generating .htaccess files whenever rewrite rules are flushed
-add_filter( 'flush_rewrite_rules_hard', '__return_false');
-
-/**
  * Alter the timeout on cron requests from 0.01 to 0.5. Something about
  * the Vagrant and/or Ubuntu setup doesn't like these self requests
  * happening so quickly.
