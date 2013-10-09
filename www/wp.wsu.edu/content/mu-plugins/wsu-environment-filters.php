f988330aef9c80a96f168e736a62291836a40605
jeremyfelt <jeremy.felt@gmail.com>
2013-10-09 16:04:38 +0200

    Move cron request filter to the mu-plugins directory
    I'm not convinced that this is necessary anymore after advances in both Vagrant and Virtualbox, so I"m commenting it out for the time being. Mostly adding it so that we can ditch the unneeded wordpress-config directory

--- www/wp.wsu.edu/content/mu-plugins/wsu-environment-filters.php
+++ www/wp.wsu.edu/content/mu-plugins/wsu-environment-filters.php
@@ -19,3 +19,14 @@ add_filter( 'got_rewrite', '__return_true', 999 );
 
 // Avoid generating .htaccess files whenever rewrite rules are flushed
 add_filter( 'flush_rewrite_rules_hard', '__return_false');
+
+/**
+ * Alter the timeout on cron requests from 0.01 to 0.5. Something about
+ * the Vagrant and/or Ubuntu setup doesn't like these self requests
+ * happening so quickly.
+ */
+//add_filter( 'cron_request', 'vvv_cron_request', 10, 1 );
+function vvv_cron_request( $cron_request ) {
+	$cron_request['args']['timeout'] = (float) 0.5;
+	return $cron_request;
+}
