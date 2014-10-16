a4a62b5fd2c61e88c57991e4cc8e53c8cd1a7f05
jeremyfelt <jeremy.felt@gmail.com>
2014-10-16 12:25:43 -0700

    Compile Nginx with OpenSSL 1.0.1j
    See major changes from 1.0.1i -> 1.0.1j
    https://www.openssl.org/news/openssl-1.0.1-notes.html

--- provision/salt/config/nginx/compile-nginx.sh
+++ provision/salt/config/nginx/compile-nginx.sh
@@ -2,13 +2,13 @@
 #
 # Compile Nginx with SPDY and Pagespeed support.
 rm -fr /tmp/nginx-1.7.6
-rm -fr /tmp/openssl-1.0.1i
+rm -fr /tmp/openssl-1.0.1j
 rm -fr /tmp/ngx_pagespeed-1.8.31.4-beta
 
 # Compile against OpenSSL to enable NPN.
 cd /tmp/
-wget http://www.openssl.org/source/openssl-1.0.1i.tar.gz
-tar -xzvf openssl-1.0.1i.tar.gz
+wget http://www.openssl.org/source/openssl-1.0.1j.tar.gz
+tar -xzvf openssl-1.0.1j.tar.gz
 
 # Provide the PageSpeed module for Nginx.
 cd /tmp/
@@ -59,7 +59,7 @@ cd /tmp/nginx-1.7.6
 --with-ipv6 \
 --with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' \
 --with-ld-opt='-Wl,-z,relro -Wl,--as-needed' \
---with-openssl=/tmp/openssl-1.0.1i \
+--with-openssl=/tmp/openssl-1.0.1j \
 --add-module=/tmp/ngx_pagespeed-1.8.31.4-beta
 
 cd /tmp/nginx-1.7.6
