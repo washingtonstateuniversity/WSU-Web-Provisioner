49a194419725d95d11b1db66ebcdc23438bbb810
jeremyfelt <jeremy.felt@gmail.com>
2014-12-03 09:18:44 -0800

    Update Nginx to 1.7.8

--- provision/salt/config/nginx/compile-nginx.sh
+++ provision/salt/config/nginx/compile-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Compile Nginx with SPDY and Pagespeed support.
-rm -fr /tmp/nginx-1.7.7
+rm -fr /tmp/nginx-1.7.8
 rm -fr /tmp/openssl-1.0.1j
 rm -fr /tmp/ngx_pagespeed-1.9.32.2-beta
 
@@ -23,9 +23,9 @@ tar -xzvf 1.9.32.2.tar.gz # expands to psol/
 # Best to get the latest mainline release. Of course, your mileage may
 # vary depending on future changes
 cd /tmp/
-wget http://nginx.org/download/nginx-1.7.7.tar.gz
-tar zxf nginx-1.7.7.tar.gz
-cd /tmp/nginx-1.7.7
+wget http://nginx.org/download/nginx-1.7.8.tar.gz
+tar zxf nginx-1.7.8.tar.gz
+cd /tmp/nginx-1.7.8
 
 ./configure \
 --user=www-data \
@@ -62,6 +62,6 @@ cd /tmp/nginx-1.7.7
 --with-openssl=/tmp/openssl-1.0.1j \
 --add-module=/tmp/ngx_pagespeed-1.9.32.2-beta
 
-cd /tmp/nginx-1.7.7
+cd /tmp/nginx-1.7.8
 make
 make install
